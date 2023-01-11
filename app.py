232ca1143c01c3ae3c00e6e968f136c6c28f59ee
Abel Romer <abel.romer@questu.ca>
2023-01-10 17:02:13 -0800

    Initial commit.

--- app.py
+++ app.py
@@ -1,35 +1,64 @@
 import os
-
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from build_latex import make_document
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+business_info = [
+            'Hiring Manager',
+            'Company',
+            'Street Address',
+            'City and ZIP'
+        ]
+
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    global business_info
     if request.method == "POST":
-        animal = request.form["animal"]
+        cover = request.form["cover"]
+        job = request.form["job"]
+        business_info = [
+            request.form["recipient"],
+            request.form["company"],
+            request.form["address"],
+            request.form["city"]
+        ]
+        allowable_response_length = 4096 - len(cover)/4 - len(job)/4
+        print(allowable_response_length)
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=generate_prompt(cover, job),
+            temperature= 0.9,
+            max_tokens=int(allowable_response_length),
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        print(response.choices)
+        print(response.choices[0])
+        return redirect(url_for("index", result=response.choices[0].text.strip('Dear Hiring Manager')))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
+@app.route("/generate_latex", methods=("GET", "POST"))
+def generate_latex():
+    if request.method == "POST":
+        completion = request.form["completion"]
+        personal_info = [
+            '1525 Griffith Park Blvd, Apt. 107',
+            'Los Angeles, CA 90026',
+            '(707) 913-7014',
+            'abel.e.romer@gmail.com',
+            'https://www.abelromer.com'
+        ]
+        global business_info
+        print(completion)
+        make_document(personal_info, business_info, completion)
+        path = '/static/cover.pdf'
+    return render_template("index.html", pdf_filepath=path)
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(cover, job):
+    prompt = f"Re-write the following cover letter using keywords from this job description: \n\nCover letter: {cover} \n\nJob description: {job}."
+    return prompt
---
+++ build_latex.py
@@ -0,0 +1,34 @@
+from pylatex import Document, Section, Subsection, Command
+from pylatex.utils import italic, NoEscape
+from pylatex.base_classes import Environment, Arguments, Options
+from pylatex.package import Package
+
+
+def teletype(text):
+    return NoEscape(r'\texttt{' + text + '}')
+
+def make_document(personal_info, business_info, content):
+    class Letter(Environment):
+        packages = []
+
+    street, city, phone, email, website = personal_info
+    recipient, company, business_street, city_zip = business_info
+
+    doc = Document(documentclass="letter")
+
+    with doc.create(Letter(arguments=Arguments(NoEscape(rf'{recipient} \\ {company} \\ {business_street} \\ {city_zip}')))):
+        doc.append(NoEscape('\opening{Dear Hiring Manager:}'))
+        doc.append(NoEscape(content))
+        doc.append(Command('closing', 'Sincerely,'))
+        doc.append(Command('encl', 'Resume'))
+
+    doc.preamble.append(Command('signature', 'Abel Romer'))
+    doc.preamble.append(Command('address', NoEscape(rf'{street} \\ {city} \\ {phone} \\ {teletype(email)} \\ {teletype(website)}')))
+
+    doc.packages.append(Package('geometry',
+                                Options(left='1.5in',
+                                        right='1.5in',
+                                        top='1.25in',
+                                        bottom='1.25in')))
+
+    doc.generate_pdf('/Users/abel/documents/programming/gpt_cover_letter/static/cover', clean_tex=False)
